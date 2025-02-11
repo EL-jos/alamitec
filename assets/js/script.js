ef814e6a026e63ef552a232cd42fd8223dab592d
EL-jos <elongajosue22@gmail.com>
2025-02-11 23:59:29 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$("#el-hero-slider .owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    smartSpeed: 1500,
+    autoplayHoverPause: true,
+    center: true,
+    responsiveClass: true,
+    responsive:{
+        0: {
+            items: 1,
+            dots: true
+        },
+    }
+});
+
+$("#el-clients .owl-carousel, #el-partners .owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    smartSpeed: 1500,
+    autoplayHoverPause: true,
+    center: true,
+    responsiveClass: true,
+    responsive:{
+        0: {
+            items: 1,
+            nav: true
+        },
+    }
+});
