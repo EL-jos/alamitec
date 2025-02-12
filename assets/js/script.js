f994e9a300fd1c3b8a8721f5bbd76f4130f66cce
EL-jos <elongajosue22@gmail.com>
2025-02-12 11:29:45 +0100

    création rubrique mines et carrières

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ $("#el-hero-slider .owl-carousel").owlCarousel({
 });
 
 $("#el-clients .owl-carousel, #el-partners .owl-carousel").owlCarousel({
-    items: 1,
+    //items: 1,
     loop: true,
     nav: false,
     dots: false,
@@ -27,6 +27,26 @@ $("#el-clients .owl-carousel, #el-partners .owl-carousel").owlCarousel({
     smartSpeed: 1500,
     autoplayHoverPause: true,
     center: true,
+    responsiveClass: true,
+    responsive:{
+        0: {
+            items: 1,
+            nav: true
+        },
+    }
+});
+
+$("#el-realisation .owl-carousel").owlCarousel({
+    //items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    smartSpeed: 1500,
+    autoplayHoverPause: true,
+    center: true,
+    margin: 10,
     responsiveClass: true,
     responsive:{
         0: {
