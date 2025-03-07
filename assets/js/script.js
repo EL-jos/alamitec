2940575b570e01e7cc43e0664eab1ec1cefc7236
EL-jos <elongajosue22@gmail.com>
2025-03-07 11:45:30 +0100

    finalisation responsive design rubrique mines et carrières

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,6 +83,21 @@ $("#el-realisation .owl-carousel").owlCarousel({
             items: 1,
             nav: true
         },
+        768: {
+            items: 2,
+            nav: true,
+            center: true
+        },
+        992: {
+            items: 3,
+            nav: true,
+            center: true,
+        },
+        1366: {
+            items: 4,
+            nav: true,
+            center: true,
+        },
     }
 });
 
